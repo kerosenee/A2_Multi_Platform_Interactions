a36689ebe9d70a788a656de2855d244174d4c622
maria kuzmenko <mariaakuzmenko@gmail.com>
2026-01-21 21:03:28 -0500

    imported some assets, built room, set up vr controls

---
+++ A2_DS3/Assets/Scripts/AnimateHands.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHands : MonoBehaviour
+{
+    public InputActionProperty triggerValue;
+    public InputActionProperty gripValue;
+    public Animator handAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float trigger = triggerValue.action.ReadValue<float>();
+        float grip = gripValue.action.ReadValue<float>();
+
+        handAnimator.SetFloat("Trigger", trigger);
+        handAnimator.SetFloat("Grip", grip);
+    }
+}
